ad03ae7f63849de80c8f5fab0d533b3c3760c8a2
Kane Davidson <kaned2001@icloud.com>
2024-04-13 11:34:23 -0700

    Added login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import Sidebar from './components/Sidebar';
 import PostDetails from './components/PostDetails';
+import Login from './pages/Login'; 
 
+// Define components for each page
+const Post = () => <h1>Post Page</h1>;
+const Blogs = () => <h1>Blogs Page</h1>;
+const Profile = () => <h1>Profile Page</h1>;
+//const Login = () => <h1>Login Page</h1>;
 
 // Component for rendering the entire blog
 const Blog = () => {
@@ -58,10 +64,26 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Blog />} />
-          {/* Add more routes as needed */}
-        </Routes>
+        <div>
+          {/* Navigation Links */}
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/post">Post</Link></li>
+              <li><Link to="/blogs">Blogs</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
+              <li><Link to="/Login">Log In</Link></li>
+            </ul>
+          </nav>
+          {/* Routes */}
+          <Routes>
+            <Route path="/" element={<Blog />} />
+            <Route path="/post" element={<Post />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
