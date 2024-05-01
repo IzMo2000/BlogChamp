5ae8ebe27b705d8474b2bda5c41d132401f3b08b
Meaghan Freund <mof25@nau.edu>
2024-05-01 15:23:48 -0700

    Adding comments to comments.jsx

--- frontend/src/pages/Comments.jsx
+++ frontend/src/pages/Comments.jsx
@@ -5,53 +5,72 @@ import { CommentFacade, UserFacade } from '../pages/Facades.js';
 import { useParams } from 'react-router-dom';
 
 const Comments = () => {
+  // initialize variables
   const { post_id } = useParams();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [commentContent, setCommentContent] = useState('');
   const [comments, setComments] = useState(null);
-  const [loading, setLoading] = useState(true); // Add loading state
+  const [loading, setLoading] = useState(true); 
   const [error, setError] = useState(null);
 
+  // get the local user id
   const storedUserId = localStorage.getItem('user_id');
 
   const fetchComments = async () => {
     try {
+      // fetch the comments from the post id
+        // from Comments Facade
       const response = await CommentFacade.fetchComments(post_id);
+      // sort the comments with the newest at the front
       const sortedComments = response.sort((a, b) => new Date(b.date_posted) - new Date(a.date_posted));
+
+      // set the fetched comments as a variable
       setComments(sortedComments);
+
+      // catch error for fetching comments
     } catch (error) {
       console.error('Error fetching comments:', error);
       setError(error.message);
     } finally {
-      setLoading(false); // Set loading to false after fetching comments
+      setLoading(false);
     }
   };
   
+  // consistently grab the comments
   useEffect(() => {
     fetchComments();
   }, [post_id]);
   
   useEffect(() => {
+    // get the users that posted each comment
     const fetchUsersForComments = async () => {
       try {
         if (comments) {
+          // update the comments
           const updatedComments = await Promise.all(comments.map(async (comment) => {
             if (!comment.user) {
+              // get the user id for the comments
               const userData = await UserFacade.fetchUserByUserid(comment.user_id);
               return { ...comment, user: userData };
             }
             return comment;
           }));
+
+          // set the comments as a variable
           setComments(updatedComments);
         }
+
+      // catch any errors
       } catch (error) {
         setError(error.message);
       }
     };
   
+  // call the function
     fetchUsersForComments();
   }, [comments]);
 
+  // open the dialogue
   const openDialog = () => {
     setIsDialogOpen(true);
   };
@@ -61,13 +80,16 @@ const Comments = () => {
     setCommentContent('');
   };
 
+  // set the comment content for the comments
   const handleCommentContentChange = (event) => {
     setCommentContent(event.target.value);
   };
 
+  // function for submitting a comment
   const handleCommentSubmit = async (event) => {
     event.preventDefault();
   
+    // comment data variables
     const commentData = {
       date_posted: new Date().toISOString(),
       post_id: post_id,
@@ -76,21 +98,28 @@ const Comments = () => {
     };
   
     try {
+      // create a comment under the given post id
       await CommentFacade.createComment(post_id, commentData);
       closeDialog();
+      // fetch the comment
       fetchComments();
+
+      // catch error for creating comment
     } catch (error) {
       console.error('Error creating comment:', error);
     }
   };
   
 
+  // set up for css
   return (
     <div className="comments-container">
+    {/*Set up the side bar on the left */}
       <div className="sidebar">
         <Sidebar />
       </div>
       <div className="main-content">
+        {/*Create a comment input */}
         <div className="create-comment">
           <button className="comment-button" onClick={openDialog}>Create Comment</button>
           {isDialogOpen && (
@@ -120,6 +149,7 @@ const Comments = () => {
           ) : comments && comments.length > 0 ? (
             comments.map((comment, index) => (
               <div key={index} className="comment">
+                {/*Show the author for each comment and the comment content */}
                 {comment.user && <p>Author: {comment.user.username}</p>}
                 <p>{comment.content}</p>
                 <p>Date Posted: {new Date(comment.date_posted).toLocaleString()}</p>
